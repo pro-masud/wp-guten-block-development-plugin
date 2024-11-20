7a9ccf0b57b0d6ef0c0a8d745f59ff15b26e4e0c
pro-masud <promasudbd@gmail.com>
2024-11-20 19:29:26 +0600

    feat: dev-block plugin block widget alingment successfully working alhdmulillah

--- dev-block/build/index.asset.php
+++ dev-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e50d898efa2089f8c5a6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '16129cb8ec5a18ea0c00');
