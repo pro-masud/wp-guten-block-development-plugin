4a6a2348fa56c115738f2c31febf3117a99fdd4f
pro-masud <promasudbd@gmail.com>
2024-11-20 22:40:17 +0600

    feat: plugin update

---
+++ block-dev/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0b05eb31d9ab6839b586');
---
+++ block-dev/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ccac0973c26d6c996ef0');
