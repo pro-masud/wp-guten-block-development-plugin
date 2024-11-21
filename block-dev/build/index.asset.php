acda203f4ce9d810962b36f1d57365fc55b0c159
pro-masud <promasudbd@gmail.com>
2024-11-21 12:38:23 +0600

    feat: update

--- block-dev/build/index.asset.php
+++ block-dev/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c9f93b11b7bcd3b01690');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e51ccdfa97e6caf58f6f');
