aa6c44125eceadbbae30a6a85856f2cc0209954e
pro-masud <promasudbd@gmail.com>
2024-11-21 23:07:06 +0600

    feat: learn-block plugin sub section customize option ready to work

---
+++ learn-block/build/blocks/about/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks', 'wp-editor'), 'version' => '418c80720e702e526c02');
---
+++ learn-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '1f4f1360fcd8c0c4383a');
